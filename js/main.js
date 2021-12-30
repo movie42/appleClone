40b19bc503904c85dd384bc5fa5d67923e6cc532
movie42 <44064122+movie42@users.noreply.github.com>
2021-12-30 16:20:54 +0900

    현재 씬에서 애니메이션 실행시 값을 계산하는 함수 playAnimation() : 각 scene에서 animation을 실행하도록 하는 함수 calcValues(): 각 scene에서 값을 받아 현재 씬의 높이 비율을 계산하여 animation을 실행하는 함수

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 // 그냥 따라만들기
 // scroll animation의 컨셉 이해하기
 // canvas 공부하기
-// 일단 프레임 워크처럼 동작하는 함수 만들기 전에 기본적인 컨셉을 이해하기
+// 스크롤 애니메이션 프레임 워크를 만들기 전에 기본적인 컨셉을 이해하기
 window.onload = (function () {
   let scrollY = 0; // window.scrollY
   let prevScrollHeight = 0; // 현재 스크롤 위치보다 이전에 위치한 스크롤 섹션들의 스크롤 높이의 합
   let currentScene = 0; // 현재 활성화 된 scene 번호
+  let enterNewScene = false; // 새로운 씬이 시작된 순간 true
   const sceneInfo = [
     {
       // 0
@@ -13,9 +14,15 @@ window.onload = (function () {
       scrollHeight: 0,
       type: "sticky",
       objs: {
-        container: document.querySelector("#scroll-section-0")
+        container: document.querySelector("#scroll-section-0"),
+        messageA: document.querySelector("#scroll-section-0 .main-message.a"),
+        messageB: document.querySelector("#scroll-section-0 .main-message.b"),
+        messageC: document.querySelector("#scroll-section-0 .main-message.c"),
+        messageD: document.querySelector("#scroll-section-0 .main-message.d")
       },
-      values: {}
+      values: {
+        messageAOpacity: [0, 1, { start: 0, end: 0.2 }]
+      }
     },
     {
       // 1
@@ -49,6 +56,35 @@ window.onload = (function () {
     }
   ];
 
+  function calcValues(values, currentScrollY) {
+    let rv;
+    // 현재 스크롤 섹션에서 스크롤 비율
+    let scrollRatio = currentScrollY / sceneInfo[currentScene].scrollHeight;
+
+    rv = scrollRatio * (values[1] - values[0]) + values[0];
+
+    return rv;
+  }
+
+  function playAnimation() {
+    const objs = sceneInfo[currentScene].objs;
+    const values = sceneInfo[currentScene].values;
+    let currentSceneScrollY = scrollY - prevScrollHeight;
+    switch (currentScene) {
+      case 0:
+        let messageAOpacityFadeIn = calcValues(values.messageAOpacity, currentSceneScrollY);
+        objs.messageA.style.opacity = messageAOpacityFadeIn;
+
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+
   function setLayout() {
     // 각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
@@ -76,7 +112,7 @@ window.onload = (function () {
     return function () {
       const context = this;
       const args = arguments;
-      console.log(context, args);
+
       let callNow = leading && !!inDebounce;
 
       const later = () => {
@@ -92,20 +128,29 @@ window.onload = (function () {
   }
 
   function scrollLoop() {
+    enterNewScene = false;
     prevScrollHeight = 0; // prevScrollHeight 초기화
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
 
     if (scrollY > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      enterNewScene = true;
       currentScene++;
       document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
 
     if (scrollY < prevScrollHeight) {
+      enterNewScene = true;
       currentScene--;
       document.body.setAttribute("id", `show-scene-${currentScene}`);
     }
+
+    if (enterNewScene) {
+      return;
+    }
+
+    playAnimation();
   }
 
   window.addEventListener("scroll", () => {
