d5e4e84a343195d8a0e3d9d76a4dfad917ae68c3
ko Hyunsu <movie3342@gmail.com>
2021-12-31 20:00:30 +0900

    캔버스가 시작되는 위치 불러오기 getBoundingClientRect()함수를 사용해서 canvas가 시작될 때, top값을 가져와 canvas가 clientHeight의 가장 위의 값에 맞을 때 애니메이션이 끝나도록하였다. 하지만 스크롤을 빨리하면 이 값 설정이 변해서 타이밍이 달라진다.

--- js/main.js
+++ js/main.js
@@ -118,6 +118,7 @@ window.onload = (function () {
       values: {
         rect1X: [0, 0, { start: 0, end: 0 }],
         rect2X: [0, 0, { start: 0, end: 0 }],
+        rectStartY: 0,
       },
     },
   ];
@@ -417,9 +418,15 @@ window.onload = (function () {
         objs.context.drawImage(objs.images[0], 0, 0);
 
         // 캔버스 사이즈에 맞춰 가정한 innerWidth와 innerHeight
-        const recalculatedInnerWidth = window.innerWidth / canvasScaleRatio;
+        const recalculatedInnerWidth =
+          document.body.offsetWidth / canvasScaleRatio;
         const recalculatedInnerHeight = window.innerHeight / canvasScaleRatio;
 
+        if (!values.rectStartY) {
+          values.rectStartY = objs.canvas.getBoundingClientRect().top;
+          values.rect1X[2].end = values.rectStartY / scrollHeight;
+          values.rect2X[2].end = values.rectStartY / scrollHeight;
+        }
         const whiteReactWdith = recalculatedInnerWidth * 0.15;
 
         values.rect1X[0] = (objs.canvas.width - recalculatedInnerWidth) / 2;
@@ -428,18 +435,33 @@ window.onload = (function () {
           values.rect1X[0] + recalculatedInnerWidth - whiteReactWdith;
         values.rect2X[1] = values.rect2X[0] + whiteReactWdith;
 
+        // 좌우 흰색 박스
+        // objs.context.fillRect(
+        //   values.rect1X[0],
+        //   0,
+        //   parseInt(whiteReactWdith),
+        //   recalculatedInnerHeight,
+        // );
+        // objs.context.fillRect(
+        //   values.rect2X[0],
+        //   0,
+        //   parseInt(whiteReactWdith),
+        //   recalculatedInnerHeight,
+        // );
+
         objs.context.fillRect(
-          values.rect1X[0],
+          parseInt(calcValues(values.rect1X, currentSceneScrollY)),
           0,
           parseInt(whiteReactWdith),
           recalculatedInnerHeight,
         );
         objs.context.fillRect(
-          values.rect2X[0],
+          parseInt(calcValues(values.rect2X, currentSceneScrollY)),
           0,
           parseInt(whiteReactWdith),
           recalculatedInnerHeight,
         );
+
         break;
     }
   }
